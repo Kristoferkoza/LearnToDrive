6036bb7d829daa435056ff202e2892e98d998bda
kfkzkozakiewicz@gmail.com <101817983+Kristoferkoza@users.noreply.github.com>
2025-02-28 14:20:14 +0100

    file-structure-updated

--- ltd-frontend/src/app/app.routes.ts
+++ ltd-frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'first-page',
+        component: FirstPageComponent,
+    },
+    {
+        path: '',
+        redirectTo: '/first-page',
+        pathMatch: 'full',
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent,
+    }
+];
